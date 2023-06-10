a479d77d5169dc9e9d30016e4ea68f8836f93d38
Pablo Rodríguez <prodrigu75@gmail.com>
2023-06-09 21:17:50 -0400

    Added zustand state manager and useRegisterModal hook

---
+++ app/hooks/useRegisterModal.tsx
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { IoMdClose } from 'react-icons/io';
+
+interface RegisterModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useRegisterModal = create<RegisterModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useRegisterModal;
