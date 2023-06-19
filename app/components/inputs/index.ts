c338458e9e794add9ca76f98a17715f00873531c
Pablo Rodríguez <prodrigu75@gmail.com>
2023-06-18 22:24:47 -0400

    Added step IMAGES & ImageUpload component

--- app/components/inputs/index.ts
+++ app/components/inputs/index.ts
@@ -1,4 +1,5 @@
 export { default as CategoryInput } from './CategoryInput';
 export { default as Counter } from './Counter';
 export { default as CountrySelect } from './CountrySelect';
+export { default as ImageUpload } from './ImageUpload';
 export { default as Input } from './Input';
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,7 +5,7 @@ import Modal from './Modal';
 import { useMemo, useState } from 'react';
 import Heading from '../Heading';
 import { categories } from '../navbar/CategoriesList';
-import { CategoryInput, Counter, CountrySelect } from '../inputs';
+import { CategoryInput, Counter, CountrySelect, ImageUpload } from '../inputs';
 import { FieldValues, useForm } from 'react-hook-form';
 import dynamic from 'next/dynamic';
 
@@ -47,6 +47,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(
     () => dynamic(() => import('../Map'), { ssr: false }),
@@ -150,6 +151,21 @@ const RentModal = () => {
           subtitle='How many bathrooms do you have?'
           onChange={(value) => setCustomValue('bathroomCount', value)}
           value={bathroomCount}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like'
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     );
