620d4627305c6deceff37e04269896ff5e67d969
Pablo Rodríguez <prodrigu75@gmail.com>
2023-06-21 20:56:29 -0400

    Added EmptyState component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import { ClientOnly, Container, EmptyState } from './components';
+
 export default function Home() {
-  return <div className='text-2xl text-rose-500'>Home</div>;
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div className='grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
